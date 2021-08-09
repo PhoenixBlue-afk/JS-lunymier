1f5ad27b82644782bbaed3e4987221cea6660673
PhoenixBlue-afk <82279633+PhoenixBlue-afk@users.noreply.github.com>
2021-08-09 14:52:06 -0300

    Add files via upload
    ciclos

---
+++ semana 2/ciclos.js
@@ -0,0 +1,11 @@
+
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 1 ) {
+        alert('Usted ha hecho click en aceptar '+ i + ' vez')
+    }else if(i=== 5){
+        alert ('estas a mitad de camino')
+    }else{
+        alert ('Usted ha hecho click en aceptar '+ i + ' veces')
+    } 
+}
